af123e85881676160a5d95b6ddfc83ce82d6a1a4
BrianHillis-DG <brian.hillis@deepgram.com>
2023-07-17 11:05:51 -0700

    Commit dist because I'm a psycho

---
+++ dist/keyboardNavigation.d.ts
@@ -0,0 +1,4 @@
+export declare const navigateUp: (path: string, shadowRoot: ShadowRoot) => void;
+export declare const navigateDown: (path: string, shadowRoot: ShadowRoot) => void;
+export declare const navigateToParent: (path: string, shadowRoot: ShadowRoot) => void;
+//# sourceMappingURL=keyboardNavigation.d.ts.map
